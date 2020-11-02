0d62f1837c8cde36a431b129ebf8e5440d97e198
Roberts Cerins <cerins4141@gmail.com>
2020-11-02 18:31:33 +0200

    Added position

---
+++ src/Position.cpp
@@ -0,0 +1,27 @@
+#include "Position.h"
+
+Position::Position(int idx, int ln, int col, std::string fn, std::string ftxt){
+    this->idx = idx;
+    this->ln = ln;
+    this->col = col;
+    this->fn = fn;
+    this->ftxt = ftxt;
+}
+
+Position::~Position(){
+
+}
+
+void Position::advance(char current_char){
+    this->idx++;
+    this->col++;
+
+    if(current_char == '\n'){
+        this->ln++;
+        this->col = 0;
+    }
+}
+
+Position Position::copy(){
+    return Position(idx, ln, col, fn, ftxt);
+}
---
+++ src/Position.h
@@ -0,0 +1,23 @@
+#ifndef POSITION_H
+#define POSITION_H
+#include <string>
+
+class Position{
+    public:
+        Position(int, int, int, std::string, std::string);
+        virtual ~Position();
+
+        int idx;
+        int ln;
+        int col;
+        std::string fn;
+        std::string ftxt;
+        void advance(char);
+        Position copy();
+
+    protected:
+
+    private:
+};
+
+#endif // POSITION_H
